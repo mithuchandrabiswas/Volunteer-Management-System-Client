64a44f3a03811bd66f032258b467b1d706c03ab9
Mithu1424882 <biswasmithumcb@gmail.com>
2024-05-11 00:45:40 +0600

    Create 404 Error Page

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,11 +3,13 @@ import Root from "../layout/Root";
 import Home from "../pages/home/Home"
 import LogIn from "../pages/authentication/LogIn";
 import Register from "../pages/authentication/Register";
+import ErrorPage from "../pages/errorPage/ErrorPage";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Root />,
+        errorElement: <ErrorPage></ErrorPage>,
         children: [
             {
                 index: true,
