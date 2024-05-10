a437cda31381c1fc6365a72ab7bed2f62ae794e4
Mithu1424882 <biswasmithumcb@gmail.com>
2024-05-11 00:15:55 +0600

    Project setup and install neccessary npm package

---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import router from './routes/Routes.jsx'
+import AuthProvider from './provider/AuthProvider.jsx'
+import { Toaster } from 'react-hot-toast'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+// Create a client
+const queryClient = new QueryClient()
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router}>
+        </RouterProvider>
+      </QueryClientProvider>
+
+      <Toaster />
+    </AuthProvider>
+  </React.StrictMode>,
+)
